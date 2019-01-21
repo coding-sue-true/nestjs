cb991caef3a429049939d185682d9d2833097b81
soraia.carmo <soraia.carmo@10xbanking.com>
2019-01-21 14:04:06 +0000

    server starting on port 3000, DB connected to AWS, simple GET / request only

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Controller, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
+  getHello(@Req() req, @Res() res): string {
     return this.appService.getHello();
   }
 }
